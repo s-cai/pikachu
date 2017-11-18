a4c4da34d10fed580cf73d98d3341e55b8e4137f
scai <you@emxample.com>
2017-11-18 12:48:56 -0500

    fix gitignore

---
+++ python/pikachu/lib/__init__.py
@@ -0,0 +1,1 @@
+"""module for generic functions"""
---
+++ python/pikachu/lib/rotation_logger.py
@@ -0,0 +1,17 @@
+import logging
+from   logging.handlers import RotatingFileHandler
+
+
+def set_root_logger(
+        filename,
+        level='INFO',
+        format="%(message)s",
+        **file_handler_kwargs):
+    """
+    Set and return the root handler to have a sole handler of `RotatingFileHandler`.
+    """
+    logging.basicConfig(level=level, format=format)
+    handler = RotatingFileHandler(filename, **file_handler_kwargs)
+    root = logging.getLogger()
+    root.handlers[:] = [handler]
+    return root
